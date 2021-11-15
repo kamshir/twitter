ca1a15e3dfa37cd441e29bf69dc5e3082ec947ee
Максим <46054418+kamshir@users.noreply.github.com>
2021-11-15 10:50:36 +0300

    Add files via upload

--- liked.php
+++ liked.php
@@ -1,9 +1,9 @@
 <?php
 include_once "includes/functions.php";
-$posts = get_posts(0, true);
-$title = 'Сначал старые посты';
+$posts = get_liked_posts();
+$title = 'Понравившиеся посты';
 $error = get_error_message();
+
 include_once "includes/header.php";
-if (logged_in()) include_once "includes/twitter_form.php";
 include_once "includes/posts.php";
 include_once "includes/footer.php";
